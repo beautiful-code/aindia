e80a6f4eb6cb916d89b55d9b6a0034dea24cbc42
Vamsee <krishna.kotla@gmail.com>
2017-05-11 14:21:50 +0530

    changes related to view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
 end
