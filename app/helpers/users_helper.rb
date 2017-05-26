cb0ba39425002c245896f7ad7f90932b59e25e3d
Vamsee <krishna.kotla@gmail.com>
2017-05-26 18:03:39 +0530

    changes to ui of profile with causes.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def image_for(user, size: 60)
     if user.image_url
       imageurl = "https://sports.ndtv.com/cricket/teams/6-india-teamprofile?s=#{size}"
-      image_tag(imageurl, alt: user.name, class: "gravatar")
+      image_tag(user.image_url, alt: user.name, class: "gravatar")
     end
   end
 
