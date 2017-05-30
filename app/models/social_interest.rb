91ad3fe9457f9ae74b45405687e877f84fd2487f
Vamsee <krishna.kotla@gmail.com>
2017-05-30 15:52:44 +0530

    Removing unnecessary code and files of old relationships

--- app/models/social_interest.rb
+++ app/models/social_interest.rb
@@ -1,12 +1,7 @@
 class SocialInterest < ApplicationRecord
   # attr_accessor :title, :description, :image_url
   validates :title, presence: true
-
   has_and_belongs_to_many :users
-  # has_many :active_relationships, class_name:  "Relationship",
-  #                                 foreign_key: "user_id",
-  #                                 dependent:   :destroy
-  # has_many :followers, through: :active_relationships, source: :user_id
 
   # def initialize(hash)
   #   hash.each do |k,v|
@@ -14,26 +9,6 @@ class SocialInterest < ApplicationRecord
   #     self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
   #     self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
   #   end
-  # end
-
-  # Follows an interest.
-  # def follow(other_user_id)
-  #   followers << other_user_id
-  # end
-
-  # TODO: Move this functionality to user.
-  #def add_follower user
-  #  followers << user
-  #end
-
-  # Unfollows an interest.
-  # def unfollow(other_user_id)
-  #   followers.delete(other_user_id)
-  # end
-
-  # Returns true if the current user is following the interest.
-  # def following?(other_user_id)
-  #   followers.include?(other_user_id)
   # end
 
 end
