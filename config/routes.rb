d554a19c33ff2990c17affb728885fb4487abfb8
Vamsee <krishna.kotla@gmail.com>
2017-05-31 20:15:37 +0530

    changes related to rspec for models and social interests

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/auth/:provider/callback', :to => 'sessions#create', via: :get
+  # match '/auth/:provider/callback' => 'sessions#callback', via: :get
 
 end
