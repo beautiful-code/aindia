3ecfa4d57a0e7f84ac2366e5fc3256923a5de337
Vamsee <krishna.kotla@gmail.com>
2017-05-31 22:02:53 +0530

    specs for causes

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -2,6 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Issue do
   let(:user) { FactoryGirl.create(:user) }
-  
+  let(:user_with_causes) { FactoryGirl.create(:user_with_issues) }
+
+  before do
+  end
+
+  it "should be valid" do
+    cause = user_with_causes.issues.first
+    expect(cause.valid?).to be_truthy
+  end
+
+  it "user id should be present" do
+    cause = user_with_causes.issues.first
+    expect(cause.user.id).not_to be_nil
+  end
+
+  it "title should be present" do
+    cause = user_with_causes.issues.first
+    expect(cause.title).not_to be_nil
+  end
+
+  it "content or image should be present" do
+    cause = user_with_causes.issues.first
+    expect([cause.content, cause.imageurl]).not_to be_nil
+  end
+
+  it "causes order should be most recent first"
+  # do
+    # causes = user_with_causes.issues
+    # cause = user_with_causes.issues.first
+    # expect(causes(:most_recent)).to be_equal(cause)
+  # end
 
 end
