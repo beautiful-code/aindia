1005982ba066bd21a8c90d65fcbdac082df21fc5
Vamsee <krishna.kotla@gmail.com>
2017-06-13 11:50:37 +0530

    config changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,57 +12,47 @@
 
 ActiveRecord::Schema.define(version: 20170609060159) do
 
-  create_table "issues", force: :cascade do |t|
-    t.text     "content"
+  create_table "issues", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "title"
     t.string   "imageurl"
     t.string   "impact"
-    t.float    "cost"
-    t.index ["user_id", "created_at"], name: "index_issues_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_issues_on_user_id"
+    t.float    "cost",       limit: 53
+    t.index ["user_id", "created_at"], name: "index_issues_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_issues_on_user_id", using: :btree
   end
 
-  create_table "issues_social_interests", id: false, force: :cascade do |t|
+  create_table "issues_social_interests", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "issue_id",           null: false
     t.integer "social_interest_id", null: false
   end
 
-  create_table "issues_users", id: false, force: :cascade do |t|
+  create_table "issues_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "issue_id", null: false
     t.integer "user_id",  null: false
-    t.index ["issue_id", "user_id"], name: "index_issues_users_on_issue_id_and_user_id"
-    t.index ["user_id", "issue_id"], name: "index_issues_users_on_user_id_and_issue_id"
+    t.index ["issue_id", "user_id"], name: "index_issues_users_on_issue_id_and_user_id", using: :btree
+    t.index ["user_id", "issue_id"], name: "index_issues_users_on_user_id_and_issue_id", using: :btree
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "interest_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["interest_id"], name: "index_relationships_on_interest_id"
-    t.index ["user_id", "interest_id"], name: "index_relationships_on_user_id_and_interest_id", unique: true
-    t.index ["user_id"], name: "index_relationships_on_user_id"
-  end
-
-  create_table "social_interests", force: :cascade do |t|
+  create_table "social_interests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "description",    limit: 65535
     t.string   "image_url"
     t.integer  "followerscount"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
-  create_table "social_interests_users", id: false, force: :cascade do |t|
+  create_table "social_interests_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "social_interest_id", null: false
     t.integer "user_id",            null: false
-    t.index ["social_interest_id", "user_id"], name: "index_social_interests_users_on_social_interest_id_and_user_id"
+    t.index ["social_interest_id", "user_id"], name: "index_social_interests_users_on_social_interest_id_and_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
@@ -76,4 +66,5 @@ ActiveRecord::Schema.define(version: 20170609060159) do
     t.string   "gender"
   end
 
+  add_foreign_key "issues", "users"
 end
