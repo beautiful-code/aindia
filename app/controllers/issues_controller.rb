c8ac805b15288954734a7e2b2f9536769c03dbba
Vamsee <krishna.kotla@gmail.com>
2017-06-14 15:09:11 +0530

    changes related to edit functionality on a cause

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,29 +1,50 @@
 class IssuesController < ApplicationController
   before_action :verify_user_has_logged_in, only: [:new, :create, :destroy]
-  before_action :verify_current_user_is_owner,   only: :destroy
+  before_action :verify_current_user_is_owner,   only: [:destroy, :edit]
 
   def new
     @issue = Issue.new
   end
 
+  def save_attributes
+    @issue.attributes = {'socialinterest_ids' => []}.merge(params[:issue] || {})
+
+    if @issue.save
+      flash[:success] = "Issue saved!"
+      redirect_to user_path(@issue.user)
+    else
+      # flash[:success] = "Issue creation failed!"
+      # redirect_to user_path
+    end
+  end
+
   def create
     @issue = current_user.issues.build(issue_params)
-    # To save the interest attributes
     @issue.attributes = {'socialinterest_ids' => []}.merge(params[:issue] || {})
 
     if @issue.save
       flash[:success] = "Issue created!"
       redirect_to root_url
     else
-      @feed_items = []
+      # flash[:success] = "Issue creation failed!"
+      # redirect_to user_path
     end
   end
 
   def destroy
-    # user = User.find(params[:id])
     @issue.destroy
     flash[:success] = "Issue deleted"
     redirect_to @current_user
+  end
+
+  def edit
+    @issue = Issue.find(params[:id])
+
+  end
+
+  def update
+    @issue = Issue.find(params[:id])
+    save_attributes
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :issues,          only: [:new, :create, :destroy]
+  resources :issues,          only: [:new, :create, :destroy, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/auth/:provider/callback', :to => 'sessions#create', via: :get
