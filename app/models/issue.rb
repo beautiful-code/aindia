0bc270f7422ce051de600aaf5ce32cc76d6eef1d
Vamsee <krishna.kotla@gmail.com>
2017-06-14 15:56:53 +0530

    changes to cause

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,7 +7,7 @@ class Issue < ApplicationRecord
 
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 140 }
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 20000 }
   has_and_belongs_to_many :socialinterests, class_name: "SocialInterest"
 
   def get_support_button_text
