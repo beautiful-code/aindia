9c3a05cd09041657ade5696cd96590a9679f566a
Vamsee <krishna.kotla@gmail.com>
2017-06-19 14:44:32 +0530

    removing unwanted functionality.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 require 'yaml'
 
 module UsersHelper
-  def image_for(user, size: 60)
-    if user.image_url
-      imageurl = "https://sports.ndtv.com/cricket/teams/6-india-teamprofile?s=#{size}"
-      image_tag(user.image_url, alt: user.name, class: "gravatar")
-    end
-  end
+  # def image_for(user, size: 60)
+  #   if user.image_url
+  #     imageurl = "https://sports.ndtv.com/cricket/teams/6-india-teamprofile?s=#{size}"
+  #     image_tag(user.image_url, alt: user.name, class: "gravatar")
+  #   end
+  # end
 end
