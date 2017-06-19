48b9d88e0c29e0430d7f1f9b00b85b237ffa28a9
Vamsee <krishna.kotla@gmail.com>
2017-06-19 16:37:11 +0530

    minor changes

--- app/controllers/concerns/support_issue.rb
+++ app/controllers/concerns/support_issue.rb
@@ -2,7 +2,6 @@ module SupportIssue
     extend ActiveSupport::Concern
 
     def update_support_issue
-      byebug
       issue = Issue.all.find(params[:issue_id])
       @current_user.update_support_issue(issue)
       redirect_to :back
